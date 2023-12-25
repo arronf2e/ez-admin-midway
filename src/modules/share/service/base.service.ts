4d19fb3ffab512c6d01c2fe13f74e1cea10fbf0a
arronf2e <arronf2e@163.com>
2023-12-25 14:54:50 +0800

    feat: add dept service

--- src/modules/share/service/base.service.ts
+++ src/modules/share/service/base.service.ts
@@ -1,7 +1,6 @@
 import { Inject } from '@midwayjs/core';
 import { RedisService } from '@midwayjs/redis';
 import { TypeORMDataSourceManager } from '@midwayjs/typeorm';
-import { Connection, EntityManager } from 'typeorm';
 
 /**
  * BaseService
